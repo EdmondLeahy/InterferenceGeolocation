e10eeafb00eb16e0ad803a0408eb7242432cc631
Edmond Leahy <edmond.leahy@hexagon.com>
2021-07-09 09:00:47 -0600

    Added file parsers

--- PBJA/Sprinkler_Geolocation.py
+++ PBJA/Sprinkler_Geolocation.py
@@ -66,6 +66,39 @@ def get_obs_from_files(obs_filenames, start_loc=1, max_size=10000):
     return obs_arrays
 
 
+def make_obs_arrays(pos_log_filename, sprinkler_log_filename):
+    with open(pos_log_filename) as bestpos_file:
+        bestpos_data = bestpos_file.readlines()
+
+    with open(sprinkler_log_filename) as sprinkler_file:
+        sprinkler_data = sprinkler_file.readlines()
+
+    pre_df_bestpos_data = [n.split(',') for n in bestpos_data]
+    pre_df_sprinkler_data = [n.split(',') for n in sprinkler_data]
+
+    bestpos_df = pd.DataFrame(pre_df_bestpos_data)
+    sprinkler_df = pd.DataFrame(pre_df_sprinkler_data)
+
+    pos_df = bestpos_df[[5, 6, 11, 12, 13]]
+    pos_df.columns = ['Week', 'GPSTime', 'Lat', 'Long', 'Height']
+    pos_df = pos_df.set_index('GPSTime')
+
+    sprklr_df = sprinkler_df.drop([0, 1, 2, 3, 4, 7, 8, 9, 1035], axis=1)
+    sprklr_df = sprklr_df.set_index(6)
+
+    merged_df = pd.merge(pos_df, sprklr_df, left_index=True, right_index=True)
+
+    return merged_df
+
+
+def add_ENU(merged_df, expansion):
+    # Convert to ENU
+    merged_df['E'] = merged_df.apply(lambda row: convert_to_ENU(row, expansion)[0], axis=1)
+    merged_df['N'] = merged_df.apply(lambda row: convert_to_ENU(row, expansion)[1], axis=1)
+    merged_df['U'] = merged_df.apply(lambda row: convert_to_ENU(row, expansion)[2], axis=1)
+
+    return merged_df
+
 def get_series_from_log(log):
     """ Function to return a series from an individual sprinkler log """
     series = [int(dat) for dat in log[8:-4]]
