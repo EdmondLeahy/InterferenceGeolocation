6f61642115bc42d879bf305f269605ece4847dd8
Edmond Leahy <edmond.leahy@hexagon.com>
2021-07-09 11:36:18 -0600

    converter module functionally complete

--- PBJA/converter.py
+++ PBJA/converter.py
@@ -57,9 +57,74 @@ def ecef_to_enu(ecef, geodetic_0):
     return e, n, u
 
 
-def convert_to_ENU(row, expansion):
-    enu = geodetic_to_enu(expansion, (float(row['Lat']), float(row['Long']), float(row['Height'])))
+def geodetic_to_ecef(geodetic, datum=DATUMS['WGS84']):
+    """Geodetic to ECEF. Convert Geodetic lat, long, height to ECEF x, y ,z.
+
+    Note: Height must be ellipsoidal height
+
+    Args:
+        geodetic (list of float): Geodetic coordinate in form [lat, long, height]
+        datum (:obj:`Datum`): Datum(ellipsoid) to use for conversion constants (defaults to WGS84)
+
+    Returns:
+        (tuple of float): ECEF coordinate in form (x, y, z)
+
+    STOLEN FROM NOVATEL_COORDINATES.TRANSFORMATIONS!
+
+    """
+
+    lat = geodetic[0]
+    lon = geodetic[1]
+    h = geodetic[2]
+
+    lam = np.radians(lat)  # Latitude in Radians
+    phi = np.radians(lon)  # Longitude in Radians
+    s = np.sin(lam)
+    n = datum.ellipsoid.a / np.sqrt(1 - datum.ellipsoid.e_sq * s * s)
+
+    sin_lambda = np.sin(lam)
+    cos_lambda = np.cos(lam)
+    cos_phi = np.cos(phi)
+    sin_phi = np.sin(phi)
+
+    x = (h + n) * cos_lambda * cos_phi
+    y = (h + n) * cos_lambda * sin_phi
+    z = (h + (1 - datum.ellipsoid.e_sq) * n) * sin_lambda
+
+    return x, y, z
+
+
+def geodetic_to_enu(geodetic, geodetic_0, datum=DATUMS['WGS84']):
+    """Geodetic To ENU
+
+    Convert Geodetic lat, long, height to ENU e, n, u about geodetic_0 lat_0, long_0, height_0.
+
+    Heights must be ellipsoidal heights.
+
+    Args:
+        geodetic (list of float): Geodetic coordinate in form [lat, long, height]
+        geodetic_0 (list of float): Geodetic coordinate in
+            form [lat_0, long_0, height_0] about which to calculate ENU
+        datum (:obj:`Datum`): Datum(ellipsoid) to use for conversion constants (defaults to WGS84)
+
+    Returns:
+        (tuple of float): ENU w.r.t geodetic_0 in
+            form (easting, northing, up)
+
+
+    STOLEN FROM NOVATEL_COORDINATES.TRANSFORMATIONS!
+
+
+    """
+    ecef = geodetic_to_ecef(geodetic, datum=datum)
+    enu = ecef_to_enu(list(ecef), geodetic_0)
     return enu
+
+
+def convert_to_ENU(row, expansion):
+    enu = geodetic_to_enu([float(row['Lat']), float(row['Long']), float(row['Height'])], expansion)
+    return enu
+
 
 def make_obs_arrays(pos_log_filename, sprinkler_log_filename):
     with open(pos_log_filename) as bestpos_file:
@@ -83,7 +148,13 @@ def make_obs_arrays(pos_log_filename, sprinkler_log_filename):
 
     merged_df = pd.merge(pos_df, sprklr_df, left_index=True, right_index=True)
 
-    return merged_df
+    expansion = [float(merged_df['Lat'][0]),
+                 float(merged_df['Long'][0]),
+                 float(merged_df['Height'][0])]
+
+    enu_added_df = add_ENU(merged_df, expansion)
+
+    return enu_added_df
 
 
 def add_ENU(merged_df, expansion):
@@ -94,8 +165,10 @@ def add_ENU(merged_df, expansion):
 
     return merged_df
 
+
 def run_nconvert(filename):
     pass
+
 
 def parse_imput_files(filepath):
 
